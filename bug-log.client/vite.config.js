8896731f9c9082b8697e012561abc6135e6b8fd3
Luc Weidell <>
2021-09-22 14:19:05 -0600

    vite fixed with npm run build

--- bug-log.client/vite.config.js
+++ bug-log.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../bug-log/client',
     sourcemap: false
   },
   server: {
---
+++ bug-log/client/assets/AboutPage.c322d42e.js
@@ -0,0 +1,1 @@
+import{o as a,a as e,b as o}from"./vendor.97125de4.js";const t={name:"AboutPage"},s={class:"about"},n=o("h1",null,"This is the about page",-1);t.render=function(o,t,r,u,d,i){return a(),e("div",s,[n])};export default t;
